5e7cf98e04293b03e5f0b168d468e7c0f817c276
Karl <karlding@users.noreply.github.com>
2017-03-11 10:00:06 -0500

    Make types/ a submodule

--- dirchromatic/logger/colour.py
+++ dirchromatic/logger/colour.py
@@ -1,8 +1,8 @@
 class Colour(object):
     NONE = ''
     RESET = '\033[0m'
-    RED = '\033[91m'
-    GREEN = '\033[92m'
+    RED = '\033[31m'
+    GREEN = '\033[32m'
     YELLOW = '\033[93m'
     BLUE = '\033[94m'
     MAGENTA = '\033[95m'
